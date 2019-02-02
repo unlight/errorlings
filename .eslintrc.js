d164a956f5d1de0c95933e43d709ff819abf8f57
roman.vasilev <root@localhost>
2019-02-03 00:50:13 +0400

    build: Replaced build system

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,34 +6,35 @@ module.exports = {
     'extends': [
         'eslint:recommended',
         'plugin:unicorn/recommended',
+        'plugin:@typescript-eslint/recommended',
     ],
-    'parser': 'typescript-eslint-parser',
+    'parser': '@typescript-eslint/parser',
     'parserOptions': {
         'ecmaVersion': 2017,
         'sourceType': 'module',
         'ecmaFeatures': {
             'jsx': false,
         },
+        'project': 'tsconfig.json',
     },
     'plugins': [
         'unicorn',
-        'typescript',
         'import',
-        'tslint',
+        '@typescript-eslint',
+        '@typescript-eslint/tslint',
     ],
     'rules': {
         'no-undef': 0,
         'no-unused-vars': 0,
         'indent': 0,
         'unicorn/import-index': 0,
-        'no-dupe-class-members': 0,
         'import/newline-after-import': 0,
         'import/no-duplicates': 1,
         'import/max-dependencies': [1, { 'max': 10 }],
         'quotes': [1, 'single', { 'allowTemplateLiterals': true }],
         'semi': [1, 'always'],
-        'tslint/config': [1, {
-            configFile: 'tsconfig.json',
+        '@typescript-eslint/explicit-member-accessibility': 0,
+        '@typescript-eslint/tslint/config': [1, {
             lintFile: './tslint.json',
         }],
     }
